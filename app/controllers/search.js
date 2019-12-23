e4dcee76bf03e0475f963aa3676beefff46f98e6
aaghajanyan <albert.aghajanyan.im@gmail.com>
2019-12-23 17:53:38 +0400

    Added search feature initial version (only with AND condition)

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,168 +1,273 @@
 const {
     user: userModel,
-    roles: rolesModel,
-    roles_relations: rolesRelationModel,
-    roles_groups: rolesGroupsModel,
     users_skills: userSkillsModel,
     users_categories: userCategoriesModel,
     skill: skillModel,
     category: categoryModel,
-    skills_relation: skillsRelationModel,
-    categories_relation: categoriesRelationModel,
-    Sequelize: Sequelize
 } = require("../sequelize/models");
 
-const Op = Sequelize.Op;
+let skillIdsList = [];
+let categoriesIdsList = [];
 
-const findData = async function(request, response, next) {
+const search = async function(request, response, next) {
     try {
-        const whereCondition = {
-            usersCondition: [],
-            categoriesCondition: [],
-            skillsCondition: [],
-            usersSkillsCondition: [],
-            usersCategoriesCondition: [],
+        skillIdsList = [];
+        categoriesIdsList = [];
+        const queryWhere = {
+            usersCondition: { $or: [], $and: [], $gte: [], $lte: [] },
+            categoriesCondition: { $or: [], $gte: [], $lte: [] },
+            skillsCondition: { $or: [], $gte: [], $lte: [] },
+            usersSkillsCondition: { $or: [], $gte: [], $lte: [] },
+            usersCategoriesCondition: { $or: [], $gte: [], $lte: [] }
         };
-        request.body.forEach(async (item) => {
-            const { type, condition, items } = item;
-            const where = collectCondition(items, condition);
-            const _ = await getModel(whereCondition, where, type, next);
-        })
-        const users = await getUsers(whereCondition);
+        for (const item of request.body) {
+            const { type, opCondition, relCondition, items } = item;
+            const currWhere = await collectCondition(items, opCondition);
+            await collectQueryWhere(queryWhere, currWhere, type, opCondition, relCondition, next);
+        }
+        const users = await getUsers(queryWhere);
         response.status(200).send({
             success: true,
-            message: 'Search',
             users: users
-        })
+        });
     } catch (error) {
-        console.log("ERROR: ", error);
-        response.status(409).send({
+        console.log(error);
+        response.status(400).send({
             success: false,
-            message: 'Could not search users.',
-        })
+            message: "Could not search data."
+        });
     }
 };
 
-const collectCondition = (items) => {
+const getCondition = (condition, fieldName) => {
+    switch (condition) {
+        case "equal":
+            if (fieldName == "experience" || fieldName == "profficience") {
+                return "$gte";
+            }
+            return "$eq";
+        case "not equal":
+            return "$ne";
+        case "greater":
+            return "$gte";
+        case "lesser":
+            return "$lte";
+        case "or":
+            return "$or";
+        case "and":
+            return "$and";
+    }
+};
+
+const collectCondition = (items, opCondition) => {
     const where = [];
-    Object.keys(items).forEach(function (item) {
+    const itemsKeys = Object.keys(items);
+    for (item of itemsKeys) {
+        const opTypeObj = {};
+        const opType = getCondition(opCondition, item);
+        opTypeObj[opType] = items[item];
         const tmpCondition = {};
-        tmpCondition[item] = items[item];
+        tmpCondition[item] = opTypeObj;
         where.push(tmpCondition);
-    });
+    }
     return where;
-}
+};
 
-const separateConditionsByType = async (conditionObj, relatedConditionObj, condition) => {
-    condition.forEach(function (item) {
-        const keyName = Object.keys(item);
-        const tmpCondition = {};
-        tmpCondition[keyName] = item[keyName];
-        if(keyName != 'name') {
-            tmpCondition[keyName] = {[Op.gte]: item[keyName]};
-            relatedConditionObj.push(tmpCondition);
-        } else {
-            conditionObj.push(tmpCondition);
+/*
+ * change key name which received from requiest.body from id to skillId / categoryId
+ * receivedType (e.g branch, skill, category ...)
+ * neededType
+ * obj
+ * $and
+ * idsList
+ */
+const changeKeyName = ( keyFromName, keyToName, receivedType, neededType, obj, $and, idsList) => {
+    obj.map(item => {
+        const keyName = Object.keys(item)[0];
+        if (keyName == "id" && receivedType == neededType) {
+            item[keyToName] = item[keyFromName];
+            delete item[keyFromName];
+            idsList.push(item[keyToName].$eq);
         }
+        $and.push(item);
     });
-}
+};
 
-const getModel = async (conditionsObj, condition, type, next) => {
-    switch(type) {
-        case 'skill':
-            separateConditionsByType(conditionsObj.skillsCondition, conditionsObj.usersSkillsCondition, condition);
-            return skillModel;
-        case 'category':
-            separateConditionsByType(conditionsObj.categoriesCondition, conditionsObj.usersCategoriesCondition, condition);
-            return categoryModel;
-        case 'user':
-            conditionsObj.usersCondition.push(condition[0]);
-            return userModel;
-        case 'branch':
-            condition[0].branchName = condition[0].name;
-            delete condition[0].name;
-            conditionsObj.usersCondition.push(condition[0]);
-            return userModel;
-        case 'position':
-            condition[0].position = condition[0].name;
-            delete condition[0].name;
-            conditionsObj.usersCondition.push(condition[0]);
-        return userModel;
+const changeKeyNameUserType = ( obj, keyFromName, keyToName, needToDelOld, finallyObj) => {
+    obj.map(item => {
+        if (needToDelOld) {
+            item[keyToName] = item[keyFromName];
+            delete item[keyFromName];
+        }
+        finallyObj.push(item);
+    });
+};
+
+const collectQueryWhere = async (queryWhere, currWhere, type, opCondition, relCondition, next) => {
+    const $and = [];
+    changeKeyName("id", "skillId", type, "skill", currWhere, $and, skillIdsList);
+    changeKeyName("id", "categoryId", type, "category", currWhere, $and, categoriesIdsList);
+
+    switch (type) {
+        case "branch":
+            changeKeyNameUserType(currWhere, "name", "branchName", true, queryWhere.usersCondition.$and);
+            break;
+        case "position":
+            changeKeyNameUserType(currWhere, "name", "position", true, queryWhere.usersCondition.$and);
+            break;
+        case "user":
+            changeKeyNameUserType(currWhere, "name", "position", false, queryWhere.usersCondition.$and);
+            break;
+        case "skill":
+            queryWhere.usersSkillsCondition.$or.push($and);
+            break;
+        case "category":
+            queryWhere.usersCategoriesCondition.$or.push($and);
+            break;
         default: {
-            let err = new Error('Invalid type to search employees');
+            let err = new Error("Invalid type to search employees");
             err.status = 400;
             next(err);
         }
     }
-}
+};
 
-const getUsers = async(conditionsByCriteria) => {
+const getWhereQueryLength = (whereQuery, isUserQuery = false) => {
+    let count = 0;
+    console.log(isUserQuery, whereQuery);
+    if (isUserQuery) {
+        return count;
+    }
+    if ("$and" in whereQuery) {
+        count += whereQuery.$and.length;
+    }
+    if ("$or" in whereQuery) {
+        count += whereQuery.$or.length;
+    }
+    if ("$gte" in whereQuery) {
+        count += whereQuery.$gte.length;
+    }
+    if ("$lte" in whereQuery) {
+        count += whereQuery.$lte.length;
+    }
+    return count;
+};
+
+const initFinallyWhereQuery = async (whereQuery, isUserQuery = false) => {
+    if (whereQuery.$or.length <= 0) {
+        delete whereQuery.$or;
+    }
+    if (isUserQuery && whereQuery.$and.length <= 0) {
+        delete whereQuery.$and;
+    }
+    if (whereQuery.$gte.length <= 0) {
+        delete whereQuery.$gte;
+    }
+    if (whereQuery.$lte.length <= 0) {
+        delete whereQuery.$lte;
+    }
+};
+
+const getUsers = async (queryWhere) => {
     const {
         usersCondition,
         categoriesCondition,
         skillsCondition,
         usersSkillsCondition,
         usersCategoriesCondition
-    } = conditionsByCriteria;
+    } = queryWhere;
 
-    const skillsConditionQuery = skillsCondition.length > 0 ? {[Op.or]: skillsCondition } : skillsCondition;
-    const categoriesConditionQuery = categoriesCondition.length > 0 ? {[Op.or]: categoriesCondition } : categoriesCondition;
+    initFinallyWhereQuery(usersCondition, true);
+    initFinallyWhereQuery(skillsCondition);
+    initFinallyWhereQuery(categoriesCondition);
+    initFinallyWhereQuery(usersSkillsCondition);
+    initFinallyWhereQuery(usersCategoriesCondition);
+
+    const userQueryCount = getWhereQueryLength(usersCondition, true);
+    const userSkillQueryCount = getWhereQueryLength(usersSkillsCondition);
+    const userCategoryQueryCount = getWhereQueryLength(
+        usersCategoriesCondition
+    );
 
     const users = await userModel.findAll({
-        where:   usersCondition,
-        required: usersCondition.length > 0 ? true : false,
-        attributes: { exclude: ["id", "password", "roleGroupId"] },
+        where: usersCondition,
+        required: userQueryCount > 0,
+        attributes: { exclude: ["password", "roleGroupId"] },
         include: [
             {
                 attributes: { exclude: ["id"] },
-                model: rolesGroupsModel,
-                as: "roleGroup",
-                required: false,
-                include: {
-                    model: rolesModel,
-                    as: "roles",
-                    attributes: ["name"],
-                    required: false,
-                    through: {
-                        model: rolesRelationModel,
-                        as: "roleRelation",
-                        attributes: []
-                    }
-                }
-            },
-            {
-                attributes: { exclude: ["id"] },
                 model: skillModel,
-                where: skillsConditionQuery,
                 as: "skills",
-                required: skillsCondition.length > 0 ? true : false,
+                required: userSkillQueryCount > 0,
                 through: {
                     model: userSkillsModel,
                     where: usersSkillsCondition,
-                    required: usersSkillsCondition.length > 0 ? true : false,
+                    required: userSkillQueryCount > 0,
                     as: "skillMark",
-                    attributes: ["profficience", "experience", "guid"]
+                    attributes: { exclude: ["id"] },
                 }
             },
             {
                 attributes: { exclude: ["id"] },
                 model: categoryModel,
-                where: categoriesConditionQuery,
                 as: "categories",
-                required: categoriesCondition.length > 0  ? true : false,
+                required: userCategoryQueryCount > 0,
                 through: {
                     model: userCategoriesModel,
                     where: usersCategoriesCondition,
-                    required: usersCategoriesCondition.length > 0  ? true : false,
+                    required: userCategoryQueryCount > 0,
                     as: "categoryMark",
-                    attributes: ["experience", "profficience", "guid"]
-                },
+                    attributes: { exclude: ["id"] },
+                }
             }
         ]
     });
-    return users;
+    return await filterUsers(users);
+};
+
+const initializeActualSkills = (users, actualSkills, actualCategories) => {
+    users.forEach(user => {
+        if (!actualSkills[user.id]) {
+            actualSkills[user.id] = [];
+        }
+        user.skills.forEach(skill => {
+            actualSkills[user.id].push(skill.id);
+        })
+
+        if (!actualCategories[user.id]) {
+            actualCategories[user.id] = [];
+        }
+        user.categories.forEach(category => {
+            actualCategories[user.id].push(category.id);
+        })
+    });
 }
 
+const filterUsers = async (users) => {
+    const actualSkills = {};
+    const actualCategories = {};
+    initializeActualSkills(users, actualSkills, actualCategories);
+
+    const  actualSkillsKeys = Object.keys(actualSkills);
+    const  actualCategoriesKeys = Object.keys(actualCategories);
+
+    if (skillIdsList.length > 0) {
+        actualSkillsKeys.map(key => {
+            if(actualSkills[key].length != skillIdsList.length)  {
+                users.splice(users.findIndex(user => user.id === key), 1);
+            }
+        })
+    }
+    if (categoriesIdsList.length > 0) {
+        actualCategoriesKeys.map(key => {
+            if(actualCategories[key].length != categoriesIdsList.length)  {
+                users.splice(users.findIndex(user => user.id === key), 1);
+            }
+        })
+    }
+    return users;
+};
+
 module.exports = {
-    findData
+    search
 };
--- app/routes/search.js
+++ app/routes/search.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const { findData } = require("../controllers/search");
-const { findDataTest } = require("../controllers/search-test");
-const { searchData } = require("../controllers/search-different-queries");
+const { search } = require("../controllers/search");
 const { validateSearchBodySchema } = require("../validation/search");
 const { verifyLoginToken } = require('../validation/token');
 const router = express.Router();
 
-router.post("/", verifyLoginToken, validateSearchBodySchema, findData);
-router.post("/v0", verifyLoginToken, validateSearchBodySchema, findDataTest);
-router.post("/v1", verifyLoginToken, validateSearchBodySchema, searchData);
+router.post("/", verifyLoginToken, validateSearchBodySchema, search);
 
 module.exports = router;
