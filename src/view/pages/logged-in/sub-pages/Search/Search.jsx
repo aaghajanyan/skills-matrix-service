9bbffb74713d4c1ff4b5d139e8fdf4b0e570666e
aaghajanyan <albert.aghajanyan.im@gmail.com>
2020-03-03 10:47:05 +0400

    Made code clean up. Separated export excel functionality.

--- src/view/pages/logged-in/sub-pages/Search/Search.jsx
+++ src/view/pages/logged-in/sub-pages/Search/Search.jsx
@@ -6,21 +6,12 @@ import { SMButton } from "view/components/SMButton";
 import { useSelector, useDispatch } from "react-redux";
 import { SMConfig } from "config";
 import { doSearch, getCriteria } from "store/actions/search";
-
+import {exportExcel} from '../../../../../helpers/exportExcel';
 import SearchTree from "./SearchTree";
-import moment from 'moment';
-
 import ReactExport from "react-data-export";
+
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
-
-const badColor = 'faad14';    // orange
-const normalColor = '1890ff'; // blue
-const goodColor = '52C41A';   // green
-const borderColor = '000';    // black
-const contentSize = '11';
-const titleSize = '11';
-const columnWidth = '150';
 
 function SearchPeople(props) {
     const [collapseFind, setCollapseFind] = useState(false);
@@ -69,93 +60,6 @@ function SearchPeople(props) {
         }
     });
 
-    const getUniqueSkillsList = (userList) => {
-        const skillsList = [];
-        userList.map(user => {
-            user.skills.map(skill => {
-                skillsList.push(skill.name)
-            })
-        });
-        return new Set(skillsList);
-
-    }
-
-    const getSkillIndex = (list, name) => {
-        let i = -1;
-        list.some((e, index) => {
-            if (e.title === name) {
-                i = index;
-                return
-            }
-        })
-        return i;
-    }
-
-    const collectSkillExcelObj = (skill) => {
-        return {
-            value: 'Exp: ' + skill.skillMark.experience +
-            '\nProf: ' + skill.skillMark.profficience +
-            '\nLDate: ' + moment(skill.skillMark.last_worked_date).format('YYYY-MM-DD'),
-            style: {
-                font: {sz: contentSize},
-                fill: {
-                    patternType: 'solid',
-                    fgColor: {rgb: skill.skillMark.profficience < 3 ? badColor : skill.skillMark.profficience === 3 ? normalColor : goodColor}
-                },
-                border: {
-                    top: {style: 'thin', color: borderColor},
-                    bottom: {style: 'thin', color: borderColor},
-                    left: {style: 'thin', color: borderColor},
-                    right: {style: 'thin', color: borderColor}
-                },
-                alignment: {
-                    vertical: 'center',
-                    horizontal: 'center'
-                }
-            }
-        };
-    }
-
-    const collectColumns = (exportingData) => {
-        const columnsLists = usersData ? exportingData[0].columns = ["Employee", "Position", "Branch", ...getUniqueSkillsList(usersData.users)] : [];
-        const columnsObj =  columnsLists.map(item => {
-            return {title: item, width: {wpx: columnWidth}, style: {font: {sz: titleSize, bold: true}, alignment: {vertical: 'center', horizontal: 'center'}}}
-        });
-        return columnsObj
-    }
-
-    const collectNameColumnsObj = (text, nameObj) => {
-        nameObj.value = text;
-        nameObj.style = {
-            font: {sz: contentSize},
-            alignment: {
-                vertical: 'center',
-                horizontal: 'center'
-            }
-        };
-    }
-
-    const downloadExcel = () => {
-        const exportingData = [
-            {
-                columns: [],
-                data: []
-            }
-        ];
-        usersData ? exportingData[0].columns = collectColumns(exportingData) : [];
-        usersData && usersData.users.forEach(user => {
-            const userResult = Array.from({ length: exportingData[0].columns.length}, () => {return {value: '', style: {}}});
-                collectNameColumnsObj(user.fname + ' ' + user.lname, userResult[0]);
-                collectNameColumnsObj(user.position.name, userResult[1]);
-                collectNameColumnsObj(user.branch.name, userResult[2]);
-                user.skills && user.skills.forEach(skill => {
-                    userResult[getSkillIndex(exportingData[0].columns ,skill.name)] = collectSkillExcelObj(skill)
-                })
-                exportingData[0].data.push(userResult);
-        });
-        return exportingData
-    };
-
     return (
         <>
             <div
@@ -169,7 +73,7 @@ function SearchPeople(props) {
                         <ExcelFile filename="findEmployees" element={<SMButton className="sm-button skills-table-add-skill-button">
                             {SMConfig.search.search.buttons.export}
                             </SMButton>}>
-                            <ExcelSheet dataSet={downloadExcel()} name="employees"/>
+                            <ExcelSheet dataSet={exportExcel(usersData)} name="employees"/>
                         </ExcelFile>
                     </div>
                 </Row>
