ba69a02058fcf18faa06be9d44b89ad609c9dd5d
aaghajanyan <albert.aghajanyan.im@gmail.com>
2020-03-03 11:52:58 +0400

    Corrected functionality to get dashboard info.

--- app/models/dashboardInfo.js
+++ app/models/dashboardInfo.js
@@ -114,7 +114,7 @@ class DashboardInfo {
                         guid: item.guid,
                         name: item.name,
                         icon: item.icon,
-                        categories: item.categories[0].name,
+                        categories: item.categories.length && item.categories[0].name,
                         last_worked_date: item.users_skills.last_worked_date,
                         profficience: profficienceObj,
                         experience: item.users_skills.dataValues.experience
@@ -133,7 +133,7 @@ class DashboardInfo {
                     const skillsInfo = {
                         guid: item.guid,
                         name: item.name,
-                        categories: item.categories[0].name,
+                        categories: item.categories.length && item.categories[0].name,
                         last_worked_date: item.users_skills.last_worked_date,
                         icon: item.icon,
                         profficience: profficienceObj,
