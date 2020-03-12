fbe68ea6d9c71ec3289ff122997a2e337999015c
aaghajanyan <albert.aghajanyan.im@gmail.com>
2020-03-12 11:18:48 +0400

    Fixed logger issues.

--- app/helper/logger.js
+++ app/helper/logger.js
@@ -2,38 +2,66 @@
 const { createLogger, format, transports } = require('winston');
 require('winston-daily-rotate-file');
 const fs = require('fs');
+const path = require('path');
+
 const env = process.env.NODE_ENV || 'development';
 const logDir = '.log';
 
 // Create the log directory if it does not exist
 if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir);
+  fs.mkdirSync(logDir);
 }
 
-const dailyRotateFileTransport = new transports.DailyRotateFile({
-    filename: `${logDir}/%DATE%-results.log`,
-    datePattern: 'YYYY-MM-DD',
-});
+class LoggerService {
+    dailyRotateFileTransport = new transports.DailyRotateFile({
+        filename: `${logDir}/%DATE%-results.log`,
+        datePattern: 'YYYY-MM-DD'
+    });
 
-const logger = createLogger({
-    // change level if in dev environment versus production
-    level: env === 'development' ? 'verbose' : 'info',
-    format: format.combine(
+    logFormat = format.printf(({ level, message, stack, timestamp, label }) => {
+        const logStack = stack ? `\n    Handled error stack:${stack}` : '';
+        return `${timestamp} ${level} [${label}]: ${message} ${logStack}`;
+    });
+
+    consoleLogFormat = format.printf(({ level, message, stack, timestamp, label }) => {
+        return `${timestamp} ${level} [${label}]: ${message}`;
+    });
+
+    logger = createLogger({
+      // change level if in dev environment versus production
+      level: env === 'development' ? 'verbose' : 'info',
+      format: format.combine(
+        format.label({ label: path.basename(process.mainModule.filename) }),
         format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss',
+          format: 'YYYY-MM-DD HH:mm:ss'
         }),
-        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-    ),
-    transports: [
+        this.logFormat
+      ),
+      transports: [
         new transports.Console({
-            level: 'info',
-            format: format.combine(
-                format.colorize(),
-                format.printf(info => `${info.timestamp} ${info.level}: ${info}`)
-            ),
+          level: 'info',
+          format: format.combine(
+            format.colorize(),
+            this.consoleLogFormat
+          )
         }),
-        dailyRotateFileTransport,
-    ],
-});
+        this.dailyRotateFileTransport
+      ]
+    });
+
+    async error(message) {
+        this.logger.error('', message);
+    }
+
+    async info(message) {
+        this.logger.info(message);
+    }
+
+    async debug(message) {
+        this.logger.debug(message);
+    }
+}
+
+const logger = new LoggerService();
 
 module.exports = logger;
